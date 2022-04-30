bdd6b2473468e411ec5ece92b3e9ed646e9820f9
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-04-30 18:35:37 -0300

    Added a camera tracking controller

---
+++ Assets/Hernan/Scripts/Camera/h_CameraController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class h_CameraController : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, -5);
+    }
+}
