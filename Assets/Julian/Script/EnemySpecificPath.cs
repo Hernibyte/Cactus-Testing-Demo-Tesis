5c0a8a95d94c1b000b617b2657a15e46c4e942cb
Julian Bega <julian.begamilrad@gmail.com>
2022-05-03 13:51:12 -0300

    EnemySpecificPath
    enemy follows a specific path for example a moving saw

--- Assets/Julian/Script/EnemySpecificPath.cs
+++ Assets/Julian/Script/EnemySpecificPath.cs
@@ -4,30 +4,21 @@ using UnityEngine;
 
 public class EnemySpecificPath : MonoBehaviour
 {
-    public List<Transform> positions;
-    int actualPos = 0;
-     float speed;
-
-    float t = 0;
-    public float timeToReachTarget;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] private float speed;
+    [SerializeField] private List<Vector3> positions;
+    private int index;
 
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime / timeToReachTarget;
-        transform.position = Vector3.LerpUnclamped(transform.position,positions[actualPos].position, Time.deltaTime * t);
-        if (transform.position.x <= positions[actualPos].position.x+0.05f && 
-            transform.position.x >= positions[actualPos].position.x - 0.05f && 
-            transform.position.y <= positions[actualPos].position.y + 0.05f && 
-            transform.position.y >= positions[actualPos].position.y - 0.05f)
+        transform.position = Vector2.MoveTowards(transform.position, positions[index], Time.deltaTime * speed);
+        if (transform.position == positions[index])
         {
-            actualPos++;
-            if (actualPos >= positions.Count) actualPos = 0;
-            t = 0;
+            if (index == positions.Count - 1)
+            {
+                index = 0;
+            }
+            else { index++; }
         }
     }
 }
