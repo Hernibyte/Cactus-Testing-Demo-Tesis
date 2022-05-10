04a8e3553998237f59a0785c8b3b88f4a50a11c9
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-05-09 23:14:30 -0300

    Changed player animation controller location

---
+++ Assets/Hernan/Scripts/Player/Anim/PlayerAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomController 
+{
+    public class PlayerAnimation : MonoBehaviour 
+    {
+        [SerializeField] SpriteRenderer playerBody;
+        h_PlayerMovement playerMovement;
+
+        void Awake()
+        {
+            playerMovement = GetComponent<h_PlayerMovement>();
+
+            playerMovement.lookLeft_Event.AddListener(LookToLeft);
+            playerMovement.lookRight_Event.AddListener(LookToRight);
+        }
+
+        void LookToLeft()
+        {
+            playerBody.flipX = false;
+        }
+
+        void LookToRight()
+        {
+            playerBody.flipX = true;
+        }
+    }
+}
