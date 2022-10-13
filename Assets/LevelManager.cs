87f3d16e3d02a764610d6029a830ec30696b454c
Julian Bega <julian.begamilrad@gmail.com>
2022-10-13 19:42:35 -0300

    Timer

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    public float timer;
+    public TextMeshProUGUI actualTime;
+    public int min;
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 60)
+        {
+            min++;
+            timer -= 60;
+        }
+        actualTime.SetText(min.ToString() + " : " + timer.ToString("F2"));
+    }
+}
