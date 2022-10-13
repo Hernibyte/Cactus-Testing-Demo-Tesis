ef25aa5ea8feb8346e57c17549b8a089686ce9e0
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-10-13 20:37:46 -0300

    Added options menu

--- Assets/Hernan/Scripts/Menu/h_MenuManager.cs
+++ Assets/Hernan/Scripts/Menu/h_MenuManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Runtime.CompilerServices;
+using TMPro;
 
 public class h_MenuManager : MonoBehaviour
 {
@@ -21,8 +23,40 @@ public class h_MenuManager : MonoBehaviour
         SceneManager.LoadScene("h_Menu");
     }
 
+    public void Options()
+    {
+        options_Panel.SetActive(!options_Panel.activeSelf);
+    }
+
+    public void ApplyNewRes()
+    {
+        Resolution newRes;
+        bool si = reses.TryGetValue(options_Dropdown.value, out newRes);
+
+        if (si) Screen.SetResolution(newRes.width, newRes.height, true);
+    }
+
     public void Exit()
     {
         Application.Quit();
     }
+
+    private void Awake()
+    {
+        List<string> reses_string = new List<string>();
+        int i = 0;
+
+        foreach (Resolution res in Screen.resolutions)
+        {
+            reses.Add(i, res);
+            ++i;
+
+            reses_string.Add(res.width + "x" + res.height);
+        }
+        options_Dropdown.AddOptions(reses_string);
+    }
+
+    [SerializeField] private GameObject options_Panel;
+    [SerializeField] private TMP_Dropdown options_Dropdown;
+    private Dictionary<int, Resolution> reses = new Dictionary<int, Resolution>();
 }
