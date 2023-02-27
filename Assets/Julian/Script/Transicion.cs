56d670d937a8ab445eaa4aeff5d552ad1de4b939
Julian Bega <julian.begamilrad@gmail.com>
2023-02-27 00:01:59 -0300

    TransicionScenes

---
+++ Assets/Julian/Script/Transicion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transicion : MonoBehaviour
+{
+    public string nextLevel;
+    public float timeToChange;
+    //
+
+    void Start()
+    {
+        StartCoroutine(ChangeScenes());
+    }
+
+    IEnumerator ChangeScenes()
+    {
+        yield return new WaitForSeconds(timeToChange);
+        SceneManager.LoadScene(nextLevel);
+    }
+}
